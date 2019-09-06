207beca5d106cdd8ac8f534844198e14bc8cfdce
fps <universityforfang@gmail.com>
2019-09-06 15:26:20 +0800

    Added override us function
    This is so that people can just use the machine when it is empty even if the previous user forgot to indicate on the machine

--- washTestBot.py
+++ washTestBot.py
@@ -10,7 +10,10 @@ nowTime = datetime.datetime.now().time()
 
 actionWord = ""
 actionWord2 = ""
+useCheckNumber = ""
+
 alreadyOpened = False
+lastUpdated = now 
 
 #t1 is the initial time
 #t2 is the current time
@@ -25,7 +28,7 @@ def sendReminder(mArray):
     for i in mArray:
         machineUser = i[0]
         if machineUser != 0 and not i[2]:
-            if diffInTime(i[1], datetime.datetime.now())[0] > 1:
+            if diffInTime(i[1], datetime.datetime.now())[0] > 120:
                 telegram_bot.sendMessage(machineUser, "Your laundry has been in the machine for more than 2hrs! It may have already been completed")
                 i[2] = True
 
@@ -37,9 +40,12 @@ def stringToBool(s):
         return False
 
 def action(msg):
+    global useCheckNumber
     global actionWord2
     global actionWord
     global alreadyOpened
+    global lastUpdated
+
     chat_id = msg['chat']['id']
     command = msg['text']
 
@@ -53,7 +59,7 @@ def action(msg):
             contents =rf.read()
             info = contents.split("|")
             print(info)
-            for i in range(0,5):
+            for i in range(0,7):
                 du = info[i*3]
                 if du == "0":
                     machineStatus[i][0] = 0
@@ -70,6 +76,7 @@ def action(msg):
     print (nowTime)
 
     keyboard = ReplyKeyboardMarkup(keyboard=[['/start', '/done'], ['/use', '/status'], ["/notify", "/reset"]])
+    numberkeyboard = ReplyKeyboardMarkup(keyboard=[['1', '2', '3'], ['4', '5', '6'], ['7', '/reset']])
     if command != "/reset":
         if actionWord == "" and actionWord2 == "":
             
@@ -79,6 +86,7 @@ def action(msg):
                 message = message + "/done - When you are done using the machine\n"
                 message = message + "/use - When you want to use a washing machine\n"
                 message = message + "/notify - Notify the person that his/her laundry is ready for collection\n"
+                message = message + "/reset - go back to the main page\n"
                 message = message + "/status - Check the status of the laundrette\n\n"
                 message = message + "Your small gesture would make it more convienient for everyone in the block."
                 
@@ -100,26 +108,33 @@ def action(msg):
                 # else:
                 #     message = "You are not using any machines at the moment!"
                 message = "Which machine is done?"
-                keyboard = ReplyKeyboardMarkup(keyboard=[['1', '2', '3'], ['4', '5', '/reset']])
+                keyboard = numberkeyboard
                 actionWord = "done"
 
             if "/use" in command:
                 message = "Which machine would you like to use?"
-                keyboard = ReplyKeyboardMarkup(keyboard=[['1', '2', '3'], ['4', '5', '/reset']])
+                keyboard = numberkeyboard
                 actionWord = "use"
 
             if "/status" in command:
                 message = ""
-                for x in range(5):
+                for x in range(7):
                     a = str(x + 1)
                     if(machineStatus[x][0] == 0):
                         message = message + a + " is Available\n"
                     else:
                         message = message + a + " is Unavailable (Time used: " + str(diffInTime(machineStatus[x][1], datetime.datetime.now())[0]) + " minutes)\n"
-            
+                message = message + "\n"
+                for i in range(7):
+                    mach = machineStatus[i]
+                    if mach[0] == chat_id:
+                        message = message + "Currently using: " + str(i+1) + "\n"
+                s2 = lastUpdated.strftime("%d/%m/%Y, %H:%M:%S")
+                message = message + "\n"
+                message = message + "Last Updated: " + s2
             if"/notify" in command:
                 message = "Who would you like to notify?"
-                keyboard = ReplyKeyboardMarkup(keyboard=[['1', '2', '3'], ['4', '5', '/reset']])
+                keyboard = numberkeyboard
                 # keyboard = ReplyKeyboardMarkup(keyboard=[['/surprise'], ['/expected']])
                 actionWord = "notify"
             
@@ -151,20 +166,25 @@ def action(msg):
                         machineStatus[machineNumber - 1][0] = 0
                         machineStatus[machineNumber - 1][1] = 0
                         machineStatus[machineNumber - 1][2] = False
+                        lastUpdated = datetime.datetime.now()
 
                     else:
                         message = "These are not your clothes!"
 
             elif actionWord == "use":
                 machineNumber = int(command)
-                if machineStatus[machineNumber - 1][0] !=0:
-                    message = "Sorry, this machine is currently being used."
-                
+                if machineStatus[machineNumber - 1][0] != 0:
+                    message = "Is the machine empty and the person forgot to indicate?"
+                    keyboard = ReplyKeyboardMarkup(keyboard=[["yes"], ["no"], ["/reset"]])
+                    actionWord2 = "useCheck"
+                    useCheckNumber = machineNumber
                 else:
                     message =  "Remember to come back when you receive the done message"
                     machineConfig = machineStatus[machineNumber - 1]
                     machineConfig[0] = chat_id
                     machineConfig[1] = datetime.datetime.now()
+                    machineConfig[2] = False
+                    lastUpdated = datetime.datetime.now()
 
             elif actionWord == "notify":
                 machineNumber = int(command)
@@ -206,6 +226,19 @@ def action(msg):
             #             telegram_bot.sendMessage(machineStatus[machineNumber - 1][0], "Your clothes are done. Do collect them soon as some one else may need to use it")
             #             message =  "We have notified the user, please give the user 5mins to arrive."
             #  else:
+            if actionWord2 == "useCheck":
+                if command == "yes":
+                    machineNumber = useCheckNumber
+                    telegram_bot.sendMessage(machineStatus[machineNumber - 1][0], "It seems that you are done with the machine.\n Please remember to let me know next time!", reply_markup=keyboard)
+                    machineStatus[machineNumber - 1][0] = chat_id
+                    machineStatus[machineNumber - 1][1] = datetime.datetime.now()
+                    machineStatus[machineNumber - 1][2] = False
+                    lastUpdated = datetime.datetime.now()
+                    message = "Okay, we have updated to you being the user of the machine! Remember to collect your clothes! :)"
+
+                if command == "no":
+                    message = "These are not your clothes!"
+
             actionWord = ""
             actionWord2 = ""
             keyboard = ReplyKeyboardMarkup(keyboard=[['/start', '/done'], ['/use', '/status'], ["/notify", "/reset"]])
@@ -221,7 +254,7 @@ def action(msg):
         telegram_bot.sendMessage(chat_id, message, reply_markup=keyboard)
 
     f= open("status.txt","w+")
-    for i in range(0,5):
+    for i in range(0,7):
         chat_ID = machineStatus[i][0]
         startTime = machineStatus[i][1]
         hasNotified = machineStatus[i][2]
@@ -230,12 +263,13 @@ def action(msg):
     f.close()
 
 
-machineStatus = [[0,0,False],[0,0, False],[0,0, False],[0,0, False],[0,0, False]]
+machineStatus = [[0,0,False],[0,0, False],[0,0, False],[0,0, False],[0,0, False],[0,0,False],[0,0,False]]
     
 schedule.every(10).seconds.do(sendReminder, machineStatus)
 
-
-telegram_bot = telepot.Bot('989321353:AAHpC8w6BAcfj6NM9Nz5hQuQF7KUl_Oj8-0')
+testBot = telepot.Bot('989321353:AAHpC8w6BAcfj6NM9Nz5hQuQF7KUl_Oj8-0')
+mainBot = telepot.Bot('930788863:AAGbxJ4CwV-z8hCjky0lqE13Cgda-3S59qc')
+telegram_bot = testBot
 print(telegram_bot.getMe())
 
 MessageLoop(telegram_bot, action).run_as_thread()
